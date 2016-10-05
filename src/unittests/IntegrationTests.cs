b0d292ff65eb4f4281773f8155b1f09d3c15279d
gmreburn <gmreburn+git@gmail.com>
2016-10-05 17:07:11 -0500

    add dummy test so restsharp is referenced by travis/nunit runner

---
+++ src/unittests/IntegrationTests.cs
@@ -0,0 +1,15 @@
+﻿namespace mattberther.chef.unittests
+{
+    using NUnit.Framework;
+    using RestSharp;
+
+    [TestFixture]
+    public class IntegrationTests
+    {
+        [Test]
+        public void test1()
+        {
+            var client = new RestClient();
+        }
+    }
+}
